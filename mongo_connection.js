6154235c28ea0f171d81828eee9a4b17a227c9df
kimmoperala <kimmo.perala@metropolia.fi>
2019-11-29 12:22:49 +0200

    Lisää funktioita MongoDB-yhteyteen

--- mongo_connection.js
+++ mongo_connection.js
@@ -22,13 +22,13 @@ async function createListing(client, newListing) {
 
 
 //READALL
-async function findOAllListings(client) {
+async function findAllListings(client) {
   const cursor = await client.db('swim_halls').collection('halls_capital_area').find();
   const result = await cursor.toArray();
   console.log(result);
 }
 
-//READCITY
+//READCITY EXAMPLE
 async function findByCity(client) {
   const cursor = await client.db('swim_halls').collection('halls_capital_area').find({kaupunki: "Helsinki"});
   const result = await cursor.toArray();
@@ -49,7 +49,7 @@ async function findByOr(client) {
   console.log(result);
 }
 
-//READWITHLIST
+//READOPTIONS (OR) VERSION 2
 async function findByList(client) {
   const cursor = await client.db('swim_halls').collection('halls_capital_area').find({hinta: { $in: [6.7, 5.5]}});
   const result = await cursor.toArray();
@@ -61,6 +61,12 @@ async function findByLimits(client){
   const cursor = await client.db('swim_halls').collection('halls_capital_area').find({alehinta: {$lte: 2.9}});
   const result = await cursor.toArray();
   console.log(result);
+}
+
+//READVALUE (shows only single values, does not show duplicates!!)
+async function findValue(client){
+  const value = await client.db('swim_halls').collection('halls_capital_area').distinct("nimi", {alehinta: 3.3});
+  console.log(value);
 }
 
 //READ
@@ -83,6 +89,29 @@ async function updateListingByName(client, nameOfListing, updatedListing){
   console.log(`${result.modifiedCount} document(s) was/were updated.`);
 }
 
+//UPDATEMANY(does not change old value if the key already existed)
+async function updateManyListings(client) {
+  result = await client.db('swim_halls').collection('halls_capital_area').updateMany({ kommentit: { $exists: false } },
+      { $set: { kommentit: [] } });
+  console.log(`${result.matchedCount} document(s) matched the query criteria.`);
+  console.log(`${result.modifiedCount} document(s) was/were updated.`);
+}
+
+//UPDATEFIELD
+async function updateField(client, idOfListing, updateInfo){
+  result = await client.db('swim_halls').collection('halls_capital_area').updateOne({_id: idOfListing}, {$push: {kommentit: updateInfo}});
+  console.log(`${result.matchedCount} document(s) matched the query criteria.`);
+  console.log(`${result.modifiedCount} document(s) was/were updated.`);
+}
+
+//DELETEFIELD
+async function deleteField(client, idOfListing){
+  await client.db('swim_halls').collection('halls_capital_area').updateOne({_id: idOfListing}, {$unset: {"kommentit.0": 1}});
+  result = await client.db('swim_halls').collection('halls_capital_area').updateOne({_id: idOfListing}, {$pull: {"kommentit": null}});
+  console.log(`${result.matchedCount} document(s) matched the query criteria.`);
+  console.log(`${result.modifiedCount} document(s) was/were updated.`);
+}
+
 //DELETE
 async function deleteListingByName(client, numberOfListing){
   result = await client.db('swim_halls').collection('halls_capital_area').deleteOne({_id: numberOfListing});
@@ -99,31 +128,36 @@ async function main() {
     //await findByLimits(client);
     //await findByCity(client);
     //await listDatabases(client);
-    //await findOAllListings(client);
+    //await findAllListings(client);
+    //await findValue(client);
     //await findOneListingByName(client, "Haagan uimahalli");
     //await deleteListingByName(client, 2);
+    //await updateManyListings(client);
+    //await deleteField(client, 4);
+    //await updateField(client, 4,"Kauheeeta paskannussintaa");
     /*await updateListingByName(client, "Mäkelänrinteen uimahalli", {
-      kaupunki: "Helsinki"
+          ratamäärä:8
     });
     await createListing(client,
         {
-          _id: 3,
-          nimi: 'Leppävaaran uimahalli',
+          _id: 4,
+          nimi: 'Keravan uimahalli',
           ratapituus: 25,
-          ratamäärä: 8,
-          puhelin: "0981657570",
+          ratamäärä: 6,
+          puhelin: "0403182081",
           aika: [
-            '7.00-20.00',
-            '6.00-20.00',
-            '10.00-20.00',
-            '6.00-20.00',
-            '7.00-20.00',
-            '9.00–19.00',
-            '9.00–19.00'],
-          hinta: 5.5,
+            '6.00-21.00',
+            '9.00-21.00',
+            '6.00-21.00',
+            '6.00-21.00',
+            '6.00-21.00',
+            '9.00–20.00',
+            '9.00–20.00'],
+          hinta: 6.0,
           alehinta: 3.0,
-          osoite: 'Veräjäpellonkatu 15, 02650 Espoo',
-          kaupunki: "Espoo"
+          osoite: 'Tuusulantie 45, 04200 Kerava',
+          kaupunki: "Kerava",
+          kommentit: []
         },
     );*/
   } catch (e) {
