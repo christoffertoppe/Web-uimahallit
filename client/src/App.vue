2a8832c51ff42a57939c167e2c2466d1a4726ef3
aseuna <arttuseuna@gmail.com>
2019-12-11 16:05:18 +0200

    fixed toaster, fixed bug with home view, added comments, added a couple of toasts that tell login status

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,7 +3,7 @@
     <div id="nav">
       <a class="navBar" @click="setAuthToFalse">Home</a> |
       <a class="navBar" @click="$bvToast.show('adminLoginToast')">Admin</a>
-      <b-toast id="adminLoginToast" title="Kirjaudu ylläpitosivulle" no-auto-hide>
+      <b-toast id="adminLoginToast" title="Kirjaudu ylläpitosivulle" toaster="b-toaster-top-center" no-auto-hide v-if="!authenticated">
         <form id="toastForm">
           <label>Käyttäjänimi: <input class="toastInputField" type="text" v-model="usr" required></label>
           <label>Salasana: <input class="toastInputField" type="password" v-model="pw" required></label>
@@ -32,6 +32,7 @@
        * Fires when user clicks corresponding button. Authenticates user by sending http post request to api
        * that compares user input to credentials(environment variables) given in local .env file
        * User is authenticated if api responds with "ACCEPTED", and vue router renders Admin component(s)
+       * Creates toasts according to user input that read "access denied" or "login successful"
        */
       login: function () {
 
@@ -55,8 +56,44 @@
           if(res.status === "ACCEPTED"){
             this.authenticated = true;
             this.$router.replace({path:"/admin"});
+
+              const h = this.$createElement;
+              const vNodesMsg = h(
+                  'p',
+                  { class: ['text-center', 'mb-0'] },
+                  [
+                      h("strong",{}, "Kirjautuminen onnistui!")
+                  ]
+              );
+
+              this.$bvToast.toast([vNodesMsg], {
+                  title: "Ilmoitus",
+                  toaster: "b-toaster-top-center",
+                  variant: "success",
+                  solid: false,
+                  appendToast: false
+              });
+
           }else {
             this.authenticated = false;
+
+            const h = this.$createElement;
+            const vNodesMsg = h(
+                'p',
+                { class: ['text-center', 'mb-0'] },
+                [
+                    h("strong",{}, "KÄYTTÖ ESTETTY!")
+                ]
+            );
+
+            this.$bvToast.toast([vNodesMsg], {
+              title: "Varoitus!",
+              toaster: "b-toaster-top-center",
+              variant: "danger",
+              solid: false,
+              appendToast: false
+            });
+
           }
         })
         .catch(function(error) {
@@ -71,7 +108,7 @@
        * Sets user authentication flag to false when moving from admin page
        */
       setAuthToFalse: function () {
-        this.$router.replace({path: "/"});
+        //this.$router.replace({path: "/"});
 
         if(this.$route.path !== "/") {
           this.$router.replace({path: "/"});
@@ -89,7 +126,7 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  min-height: 810px;
+  min-height: 850px;
 }
 
 label{
